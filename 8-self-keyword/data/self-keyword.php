bb69518f4496994ebe012aa141f3de6ff394adcd
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-13 02:00:49 +0700

    Self Keyword, PHP OOP

---
+++ 8-self-keyword/data/self-keyword.php
@@ -0,0 +1,26 @@
+<?php
+
+class Person
+{
+    const AUTHOR = "ARIEF KARDITYA";
+    var string $id;
+    var string $name;
+    var ?string $email = null;
+    var string $gender = "MALE";
+
+    function sayHello(?string $name)
+    {
+        switch (is_null($name)):
+            case true:
+                echo "Hi, my name is {$this->name}" . PHP_EOL;
+                break;
+            default:
+                echo "Hi $name, my name is {$this->name}" . PHP_EOL;
+        endswitch;
+    }
+
+    function infoAUTHOR()
+    {
+        echo "AUTHOR : " . self::AUTHOR . PHP_EOL;
+    }
+}
---
+++ 8-self-keyword/self.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . "/data/self-keyword.php";
+
+$person1 = new Person();
+$person1->infoAUTHOR();
+
+$person2 = new Person();
+$person2->infoAUTHOR();
